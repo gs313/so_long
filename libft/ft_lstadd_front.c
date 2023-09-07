8c98dfa8f3876cf54c4bc1200958f4ce75fbcda5
Sirapat Charukamnertkanok <scharuka@z3t1c3.42bangkok.com>
2023-09-07 23:56:33 +0700

    cleaning and norm

--- libft/ft_lstadd_front.c
+++ libft/ft_lstadd_front.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: scharuka <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: scharuka <scharuka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 23:54:17 by scharuka          #+#    #+#             */
-/*   Updated: 2022/08/19 23:59:55 by scharuka         ###   ########.fr       */
+/*   Updated: 2023/09/07 23:55:22 by scharuka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"libft.h"
+#include "libft.h"
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
